ae48adbdb7d00640ba67d50fbee67f652eb28215
html3012 <b_dana@ukr.net>
2021-08-29 00:51:10 +0300

    added hero

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,7 @@
 $(function () {
     $('.btn-burger').on('click', function () {
         $('.btn-burger__line').toggleClass('btn-burger__line--active');
-        $('.header__nav').toggleClass('header__nav--active');
+        $('.header__wrap').toggleClass('header__wrap--active');
         $('.logo-white').toggleClass('logo-white--active');
         $('body').toggleClass('lock');
     });
